226f95bbd0705bdc11cf009d9df19ab6c374d3d5
osfa <jacob.broms@prototyp.se>
2025-07-17 18:36:36 +0200

    comfy wireup 1

--- drawing/__init__.py
+++ drawing/__init__.py
@@ -1,4 +1,4 @@
 from .drawing import DrawingController
-from .comfy import ComfyUIController
+from .comfy import ComfyUIController, ImpostorConfig, create_impostor_controller
 
-__all__ = ["DrawingController", "ComfyUIController"]
+__all__ = ["DrawingController", "ComfyUIController", "ImpostorConfig", "create_impostor_controller"]
