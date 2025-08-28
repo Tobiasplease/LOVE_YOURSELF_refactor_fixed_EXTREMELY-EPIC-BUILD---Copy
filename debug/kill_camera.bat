f8ba68d10d98ff6bf8545348e1e1e4987587f42a
osfa <jacob.broms@prototyp.se>
2025-08-28 16:47:07 +0200

    clean out

--- debug/kill_camera.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Killing any stuck Python processes that might be holding the camera...
-taskkill /F /IM python.exe 2>nul
-taskkill /F /IM pythonw.exe 2>nul
-echo Done! Camera should be free now.
-pause
